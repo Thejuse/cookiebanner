f74226da33e26e267f6a50892d9732f429a1943e
Julian Seidl <julian@julianseidl.at>
2018-03-14 10:42:55 +0100

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# cookiebanner
+# Cookiebanner
 Typo3 extension to add a CookieConsent Banner
+
+## Installation
+1. Download extension _(cookiebanner)_
+2. Activate & include Extension
+3. Change Constants in Constenteditor
---
+++ cookiebanner/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Cookiebanner',
+    'description' => 'Adds a Cookieconsent-banner',
+    'category' => 'plugin',
+    'author' => 'Julian Seidl',
+    'author_email' => 'hi@jseidl.at',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
