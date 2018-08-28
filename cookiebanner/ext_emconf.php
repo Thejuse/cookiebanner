399ba1db1b8d6180c588233f924d53e66c384e7b
Julian Seidl <julian.seidl@koerbler.com>
2018-08-28 09:48:49 +0200

    Typo3 9 Support
    Added Typo3 9 Support

--- cookiebanner/ext_emconf.php
+++ cookiebanner/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
