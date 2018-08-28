b0684accbcdc7ce527ed58dff521a33a8f93eb9e
Julian Seidl <julian.seidl@koerbler.com>
2018-08-28 09:49:37 +0200

    Changed Version Support

--- cookiebanner/ext_emconf.php
+++ cookiebanner/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.99.99',
+            'typo3' => '8.7.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
